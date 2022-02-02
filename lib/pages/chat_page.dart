04725813226b4597496df83a3e6c63d140fc9964
Mayankjn25 <mayank.jain.25.1999@gmail.com>
2022-02-02 14:38:54 +0530

    added map and chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:snapchat_ui_clone/data/chat_json.dart';
+import 'package:snapchat_ui_clone/theme/colors.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({ Key? key }) : super(key: key);
@@ -11,7 +14,247 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.black,
+      appBar: getAppBar(),
+      body: getBody(),
+    );
+  }
+
+  PreferredSizeWidget? getAppBar() {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.white,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: black.withOpacity(0.1)),
+                child: const Icon(
+                  Entypo.user,
+                  color: primary,
+                  size: 28,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: black.withOpacity(0.1)),
+                child: const Icon(
+                  Feather.search,
+                  color: darkGrey,
+                  size: 23,
+                ),
+              )
+            ],
+          ),
+          const Text(
+            "Chat",
+            style: TextStyle(
+                fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: black.withOpacity(0.1)),
+                child: const Icon(
+                  Feather.user_plus,
+                  color: darkGrey,
+                  size: 20,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: black.withOpacity(0.1)),
+                child: const Icon(
+                  AntDesign.edit,
+                  color: darkGrey,
+                  size: 20,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          color: Colors.white),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Container(
+                    height: 1,
+                    decoration:
+                        BoxDecoration(color: Colors.grey.withOpacity(0.2)),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text(
+                  "Quick Add",
+                  style: TextStyle(color: blue),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  child: Container(
+                    height: 1,
+                    decoration:
+                        BoxDecoration(color: Colors.grey.withOpacity(0.2)),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Column(
+                children: List.generate(chat_data.length, (index) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, left: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: (size.width - 40) * 0.68,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            chat_data[index]['img']),
+                                        fit: BoxFit.cover)),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                width: (size.width - 95) * 0.3,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      chat_data[index]['name'],
+                                      style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w400),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(
+                                      height: 3,
+                                    ),
+                                    Text(
+                                      chat_data[index]['nickname'],
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black.withOpacity(0.5),
+                                          fontWeight: FontWeight.w400),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      "NEW CONTACT",
+                                      style: TextStyle(
+                                          fontSize: 11,
+                                          color: Colors.black.withOpacity(0.5),
+                                          fontWeight: FontWeight.w400),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: (size.width - 40) * 0.32,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    border: Border.all(color: blue)),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 12, bottom: 4, left: 12, top: 4),
+                                  child: Row(
+                                    children: const [
+                                      Icon(Icons.add, size: 18, color: blue),
+                                      SizedBox(width: 5),
+                                      Text(
+                                        "Add",
+                                        style: TextStyle(color: blue),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Icon(
+                                AntDesign.close,
+                                size: 16,
+                                color: darkGrey.withOpacity(0.7),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Divider()
+                ],
+              );
+            })),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:snapchat_ui_clone/theme/colors.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({ Key? key }) : super(key: key);
@@ -8,10 +13,254 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(11.5760029, 104.845914),
+    zoom: 15.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      floatingActionButton: getFooter(),
+      body: getBody(),
     );
   }
+
+  Widget getFooter() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, top: 60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black.withOpacity(0.3)),
+                    child: const Icon(
+                      Entypo.user,
+                      color: primary,
+                      size: 23,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black.withOpacity(0.3)),
+                    child: const Icon(
+                      Feather.search,
+                      color: Colors.white,
+                      size: 23,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                    color: darkGrey.withOpacity(0.30),
+                    spreadRadius: 20,
+                    blurRadius: 40,
+                    // changes position of shadow
+                  ),
+                ]),
+                child: Column(
+                  children: [
+                    const Text(
+                      "Phnom Penh",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.cloud,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "25 c",
+                          style: TextStyle(color: Colors.white, fontSize: 13),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black.withOpacity(0.3)),
+                child: const Icon(
+                  AntDesign.setting,
+                  color: Colors.white,
+                  size: 23,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 70,
+                width: 80,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 3,
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                        child: Center(
+                          child: Container(
+                            width: 45,
+                            height: 45,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey.withOpacity(0.5)),
+                            child: const Center(
+                              child: Icon(
+                                Entypo.user,
+                                color: Colors.grey,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: const Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            "My Bitmoji",
+                            style: TextStyle(
+                                fontSize: 11, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: 35,
+                height: 35,
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                    color: darkGrey.withOpacity(0.20),
+                    spreadRadius: 20,
+                    blurRadius: 40,
+                    // changes position of shadow
+                  ),
+                ], shape: BoxShape.circle, color: Colors.white),
+                child: const Center(
+                  child: Icon(
+                    FontAwesome.location_arrow,
+                    size: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 3,
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                        child: Center(
+                          child: Container(
+                            width: 45,
+                            height: 45,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey.withOpacity(0.5)),
+                            child: const Center(
+                              child: Icon(
+                                Entypo.user,
+                                color: Colors.grey,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: const Padding(
+                          padding: EdgeInsets.only(
+                              top: 5, bottom: 5, left: 12, right: 12),
+                          child: Text(
+                            "Friends",
+                            style: TextStyle(
+                                fontSize: 11, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getBody() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      myLocationButtonEnabled: false,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+  }
+  
 }
